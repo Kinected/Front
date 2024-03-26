840d726bee67ff07777b744581fb65b968216bb1
Antoine Maes <92756820+AntoLeRigoloo@users.noreply.github.com>
2024-03-26 13:31:53 +0100

    add api temperature

--- kinected/src/components/containers/weather/index.tsx
+++ kinected/src/components/containers/weather/index.tsx
@@ -1,40 +1,61 @@
+import Button from "@/components/button";
 import React from "react";
-import Button from "../../button"; // Assuming Button is a custom component
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Temperature from "@/../public/Temperature.svg";
+
+type Pos = {
+    latitude: number;
+    longitude: number;
+};
 
 export default function Weather() {
+    const [temperature, setTemperature] = useState(0);
+
     const API_KEY = "9e8e78c2678d8180cbc4c164765560c3"; // Correct line
-    const [temperature, setTemperature] = useState<number | null>(null);
 
-    // Function to fetch and update temperature
-    function fetchPosition() {
-        return;
-        navigator.geolocation.getCurrentPosition(async (position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-
-            console.log("Latitude is :", latitude);
-            console.log("Longitude is :", longitude);
-
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}}&lon=${longitude}&appid=${API_KEY}&units=metric`;
-
-            console.log("Fetching temperature from:", url);
-            const response = await fetch(url);
-            const data = await response.json();
-
-            console.log(data);
-            console.log(data.main.temp);
-            if (data) setTemperature(Math.round(data.main.temp));
+    function fetchPosition(): Promise<Pos> {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    console.log("Latitude is :", latitude);
+                    console.log("Longitude is :", longitude);
+                    resolve({
+                        latitude,
+                        longitude,
+                    });
+                },
+                (error) => reject(error)
+            );
         });
     }
 
+    async function fetchTemperature(latitude: number, longitude: number) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+
+        console.log("Fetching temperature from:", url);
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.main.temp);
+
+        setTemperature(Math.round(data.main.temp));
+    }
+
     useEffect(() => {
-        // fetchPosition();
+        async function fetchData() {
+            fetchTemperature(50.633333, 3.066667);
+            // const pos = await fetchPosition();
+            // fetchTemperature(pos.latitude || 53, pos.longitude || 3);
+        }
+        fetchData();
     }, []);
 
     return (
-        <Button className="origin-top-right" onClick={fetchPosition}>
-            {/* {temperature} */}26°C
+        <Button className="origin-top-right">
+            <Temperature className="w-8 h-8" />
+            {temperature}°C
         </Button>
     );
 }
