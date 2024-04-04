11ff7917e4ceebbf5c0807825fca2ab2b7665a92
Nicolo41 <72193849+Nicolo41@users.noreply.github.com>
2024-04-04 16:01:40 +0200

    rec

--- kinected/src/app/mic/page.tsx
+++ kinected/src/app/mic/page.tsx
@@ -1,15 +1,38 @@
 "use client";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function MicCapture() {
-  const [audio, setAudio] = useState<MediaStream | null>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+  const [chunks, setChunks] = useState<BlobPart[]>([]);
 
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ audio: true })
       .then((stream) => {
-        setAudio(stream);
+        const recorder = new MediaRecorder(stream);
+        setMediaRecorder(recorder);
+
+        recorder.ondataavailable = (e) => {
+          setChunks((prev) => [...prev, e.data]);
+        };
+
+        recorder.onstop = () => {
+          const blob = new Blob(chunks, { type: "audio/mp3" });
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "name.mp3";
+          a.click();
+        };
+
+        setTimeout(() => {
+          recorder.start();
+          setTimeout(() => {
+            recorder.stop();
+          }, 5000);
+        }, 5000);
       })
       .catch((err) => console.error("Error: ", err));
   }, []);
@@ -19,20 +42,26 @@ export default function MicCapture() {
       <span className="text-white text-4xl font-bold">
         Quel est votre nom ?
       </span>
-        <div className="flex h-full aspect-square flex-col bg-white rounded-2xl p-4">
-            <div className="relative w-full h-full">
-            <audio
-                autoPlay
-                controls
-                ref={audioElement => {
-                    if (audioElement) {
-                        audioElement.srcObject = audio;
-                    }
-                }}
-                className="absolute object-cover w-full h-full rounded-2xl top-0 left-0"
-            />
-            </div>
+      <div className="flex h-full aspect-square flex-col bg-white rounded-2xl p-4">
+        <div className="relative w-full h-full">
+          <audio
+            autoPlay
+            controls
+            ref={(audioElement) => {
+              if (
+                audioElement &&
+                mediaRecorder &&
+                mediaRecorder.state === "inactive"
+              ) {
+                audioElement.src = URL.createObjectURL(
+                  new Blob(chunks, { type: "audio/mp3" })
+                );
+              }
+            }}
+            className="absolute object-cover w-full h-full rounded-2xl top-0 left-0"
+          />
         </div>
+      </div>
     </div>
   );
 }
