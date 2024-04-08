a0019318a0ea772dca660ff751367d9c46e34ef2
Antoine Maes <92756820+AntoLeRigoloo@users.noreply.github.com>
2024-04-08 16:41:09 +0200

    fix chatbot

--- kinected/src/app/audio/user/chat-voc/page.tsx
+++ kinected/src/app/audio/user/chat-voc/page.tsx
@@ -8,117 +8,138 @@ import { useFaceStore } from "@/stores/faces.store";
 import { useRouter } from "next/navigation";
 import Modal from "@/components/modal";
 import AudioButton from "@/components/audioButton";
+import { getResponse } from "@/utils/requests/whisper/audio-chatbot";
 
 export default function AudioChatVocUser() {
-  const [isRecording, setIsRecording] = useState(false);
-  const [progress, setProgress] = useState(0);
-  const recordingTimeout = useRef<ReturnType<typeof setInterval> | null>(null);
-  const [time, setTime] = useState("0:00");
+    const [isRecording, setIsRecording] = useState(false);
+    const [isUserTalked, setIsUserTalked] = useState(false);
 
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
-    null
-  );
-  const userID = useFaceStore((state) => state.userID);
-  const router = useRouter();
-  const [response, setResponse] = useState<string | null>(null);
+    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+        null
+    );
+    const [average, setAverage] = useState(0);
+    const treshhold = 25;
 
-  const [question, setQuestion] = useState<string | null>(null);
+    const [response, setResponse] = useState<string | null>(null);
+    const [question, setQuestion] = useState<string | null>(null);
 
-  let chunks: BlobPart[] = [];
+    useEffect(() => {
+        navigator.mediaDevices
+            .getUserMedia({ audio: true })
+            .then((stream) => {
+                const audioContext = new AudioContext();
+                const source = audioContext.createMediaStreamSource(stream);
+                const analyser = audioContext.createAnalyser();
+                source.connect(analyser);
 
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream) => {
-        const newMediaRecorder = new MediaRecorder(stream);
-        setMediaRecorder(newMediaRecorder);
+                const newMediaRecorder = new MediaRecorder(stream);
+                setMediaRecorder(newMediaRecorder);
 
-        newMediaRecorder.ondataavailable = (e) => {
-          chunks.push(e.data);
-          if (newMediaRecorder.state === "inactive") {
-            sendAudio(new Blob(chunks, { type: "audio/mp3" }));
-          }
-        };
-      })
-      .catch((err) => console.error("Error: ", err));
-  }, []);
+                newMediaRecorder.ondataavailable = (e) => {
+                    console.log("Data available");
+                    if (newMediaRecorder.state === "inactive") {
+                        sendAudio(new Blob([e.data], { type: "audio/mp3" }));
+                    }
+                };
 
-  const startRecording = () => {};
+                const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-  const toggleRecording = (milis: number) => {
-    if (!isRecording && mediaRecorder) {
-      setIsRecording((prev) => !prev);
-      chunks = []; // Reset chunks at the start of recording
-      mediaRecorder.start();
-      setProgress(0);
-      const startTime = Date.now();
-      recordingTimeout.current = setInterval(() => {
-        const elapsed = Date.now() - startTime;
-        setProgress((elapsed / milis) * 100);
-        setTime(`0:0${((elapsed % 60000) / 1000).toFixed(0)}`);
-      }, 50) as NodeJS.Timeout;
-      setTimeout(() => {
-        setIsRecording(false);
-        clearInterval(recordingTimeout.current ?? undefined);
-        mediaRecorder.stop();
-      }, milis);
-    } else {
-      clearInterval(recordingTimeout.current ?? undefined);
-    }
-  };
+                const checkAudio = () => {
+                    analyser.getByteFrequencyData(dataArray);
+                    const sum = dataArray.reduce((a, b) => a + b);
+                    setAverage(sum / dataArray.length);
+                    requestAnimationFrame(() => checkAudio());
+                };
 
-  const sendAudio = (audioBlob: Blob) => {
-    console.log("Audio size:", audioBlob.size);
-    const reader = new FileReader();
-    reader.onloadend = async () => {
-      if (reader.result) {
-        const formData = new FormData();
-        formData.append("audio", new Blob([reader.result]), "audio.mp3");
-        console.log("Sending audio...");
-        console.log("Audio size:", formData);
+                checkAudio();
+            })
+            .catch((err) => console.error("Error: ", err));
+    }, []);
 
-        try {
-          const response = await fetch(
-            `http://localhost:8000/api/audio/chatvoc?userID=${userID}`,
-            {
-              method: "POST",
-              body: formData,
-            }
-          );
-          const data = await response.json();
-          console.log(data);
-          setResponse((a) => data.response);
-          setQuestion((a) => data.question);
-        } catch (error) {
-          console.error("Error sending audio:", error);
+    useEffect(() => {
+        if (!isUserTalked && average > treshhold * 1.5 && isRecording) {
+            setIsUserTalked(true);
+        } else if (average < treshhold && isRecording && isUserTalked) {
+            setTimeout(() => {
+                if (!mediaRecorder) return;
+                mediaRecorder.stop();
+                setIsRecording(false);
+                setIsUserTalked(false);
+            }, 1000);
         }
-      }
-    };
-    reader.readAsArrayBuffer(audioBlob);
-  };
+    }, [isRecording, isUserTalked, average]);
 
-  return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <AnimatePresence>
-        <>
-          {question && <span key="question">{question}</span>}
-          {response && <span key="answer">{response}</span>}
-        </>
-      </AnimatePresence>
-      <AnimatePresence>
-        {!question && (
-          <motion.div className="flex flex-col gap-8">
-            <span className="text-6xl font-bold text-white">
-              Quelle est votre question ?
-            </span>
-            <AudioButton
-              currentTime={time}
-              isRecording={isRecording}
-              onClick={() => toggleRecording(5000)}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
+    const toggleRecording = (milis: number) => {
+        if (!isRecording && mediaRecorder) {
+            setIsRecording((prev) => !prev);
+            mediaRecorder.start();
+        }
+    };
+
+    const sendAudio = (audioBlob: Blob) => {
+        console.log("Audio size:", audioBlob.size);
+        const reader = new FileReader();
+        reader.onloadend = async () => {
+            if (reader.result) {
+                const formData = new FormData();
+                formData.append(
+                    "audio",
+                    new Blob([reader.result]),
+                    "audio.mp3"
+                );
+                console.log("Sending audio...");
+                console.log("Audio size:", formData);
+
+                const data = await getResponse(formData);
+                setResponse((a) => data.response);
+                setQuestion((a) => data.question);
+            }
+        };
+        reader.readAsArrayBuffer(audioBlob);
+    };
+
+    return (
+        <motion.div className="w-full h-full flex flex-col items-center justify-center">
+            <div className="flex flex-col gap-24 flex flex-col items-center">
+                <div className="flex flex-col gap-8">
+                    <span className="text-6xl font-bold text-white">
+                        Quelle est votre question ?
+                    </span>
+                    <AudioButton
+                        isTooLoud={average > treshhold * 1.5 && !isRecording}
+                        isRecording={isRecording}
+                        onClick={() => toggleRecording(5000)}
+                    />
+                </div>
+                <AnimatePresence>
+                    {question && response && (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            className="flex-1 max-w-[75%] bg-white rounded-2xl p-4 px-8 flex flex-col gap-4"
+                        >
+                            <div className="flex flex-col gap-2">
+                                <span className="text-black font-medium text-2xl">
+                                    Retranscription :
+                                </span>
+
+                                <span className="text-black text-xl">
+                                    {question}
+                                </span>
+                            </div>
+                            <div className="flex flex-col gap-1">
+                                <span className="text-black font-medium text-2xl">
+                                    Réponse :
+                                </span>
+                                <span className="text-black text-xl">
+                                    {response}
+                                </span>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
+        </motion.div>
+    );
 }
